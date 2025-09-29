682df4c33df140bd777365cdb56638c57360b9f6
chemchimp <pythnchmp@gmail.com>
2025-09-29 22:52:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,29 +8,11 @@ api_secret = os.getenv("API_KEY_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-# Authenticate with X
+# Authenticate with X (formerly Twitter)
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Read tweets from tweets.txt
 with open("tweets.txt", "r") as f:
-    tweets = [line.strip() for line in f if line.strip()]
+    tweets = [line.strip() for line in f if]()
 
-# Pick one tweet at random
-tweet = random.choice(tweets)
-
-# Post the tweet
-api.update_status(tweet)
-
-print(f"Tweet posted: {tweet}")
-
-import os
-import tweepy
-
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_KEY_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
